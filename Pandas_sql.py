63c2415bf4cc18c4cc9d3fce238060a6c7070c86
Hemat-roy13 <84928051+Hemat-roy13@users.noreply.github.com>
2021-07-13 22:03:59 +0530

    Create Pandas_sql.py

---
+++ Pandas_sql.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import sqlite3
+
+"""
+Pandas with sqlite3, inbuilt python database
+"""
+
+createTable = """
+Create table student 
+(Id INTEGER, Name Varchar(20),Math REAL, Science REAL);
+"""
+
+executeSQL = sqlite3.connect(":memory:")
+executeSQL.execute(createtable)
+execute.commit()
+
+SQL_query = executeSQL.execute("select * from student")
+resultset = SQL_query.fetchall()
+
+resultset
+
+insert_record = [(10,'Jack',95,97), (29,'Tom',12,28)]
+insert_stmt = "Insert into student values (?,?,?,?)"
+exeuteSQL.execute(insert_stmt, insert_record)
+exeuteSQL.commit()
+
+SQL_query = executeSQl.execute("select * from student")
+
+resultset = SQL_Query.fetchall()
+resultset
